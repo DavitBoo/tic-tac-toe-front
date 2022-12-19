49394f8034ac82b46d207a151428792854f8f416
DavitBoo <davidmarras@gmail.com>
2022-12-19 17:56:25 +0100

    random function in order to play against cpu

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 const gameGrid = document.getElementById('game-grid')
 let blocks = document.querySelectorAll('.block')
 
+
+/* real player or cumpter constants and definitions */
+
+const real1 = document.getElementById('real-1')
+const cpu1 = document.getElementById('cpu-1')
+const real2 = document.getElementById('real-2')
+const cpu2 = document.getElementById('cpu-2')
+
+let typePlayerO = ''
+let typePlayerX = ''
+
+real1.addEventListener('click', () => {
+    real1.classList.add('icon-selected')
+    cpu1.classList.remove('icon-selected')
+    typePlayerO = 'real'
+})
+
+cpu1.addEventListener('click', () => {
+    cpu1.classList.add('icon-selected')
+    real1.classList.remove('icon-selected')
+    typePlayerO = 'cpu'
+})
+
+real2.addEventListener('click', () => {
+    real2.classList.add('icon-selected')
+    cpu2.classList.remove('icon-selected')
+    typePlayerX = 'real'
+})
+
+cpu2.addEventListener('click', () => {
+    cpu2.classList.add('icon-selected')
+    real2.classList.remove('icon-selected')
+    typePlayerX = 'cpu'
+})
+
+
+/* player names */
+
 const overlay = document.querySelector('.overlay')
 const overoverlay = document.querySelector('.overoverlay')
 const startBtn = document.querySelector('.start-game')
@@ -11,35 +49,24 @@ let playerO = ''
 let playerX = ''
 
 inputP1.addEventListener('input', () => {
-    console.log(playerO)
     playerO = inputP1.value
-    if(playerO !== '' && playerX !== ''){
+    if((playerO !== '' && playerX !== '') && (typePlayerO !== '' && typePlayerX !== '')) {
         startBtn.classList.remove('hide')
     }
 })
 
 inputP2.addEventListener('input', () => {
     playerX = inputP2.value
-    if(playerO !== '' && playerX !== ''){
+    if((playerO !== '' && playerX !== '') && (typePlayerO !== '' && typePlayerX !== '')){
         startBtn.classList.remove('hide')
     }
 })
 
 
-startBtn.addEventListener('click', () => {
-    overlay.classList.add('hide')
-    overoverlay.classList.add('hide')
-    startBtn.classList.add('hide')
-
-    Game.getPlayer();
+startBtn.addEventListener('click', () => {  
+    Game.startGame();
 })
 
-
-blocks.forEach(element => {
-    element.addEventListener('click', () => {
-        Gameboard.renderContent(element.dataset.block);
-    })
-})
 
 
 /**************** Game **************** */
@@ -52,9 +79,7 @@ const Game = (() => {
 
         if(player === 1) player=2;
         else player=1
-        
-        console.log(player)
-        
+                
     }
 
     const getPlayer = () => {
@@ -75,7 +100,7 @@ const Game = (() => {
                 (gameboard[2]==='X' && gameboard[4]==='X' && gameboard[6]==='X')
         ){
             console.log('X win')
-            displayPlayer.innerHTML = `Congratulations ${playerX} <br><button id="play-again">Play Again!</button>`
+            displayPlayer.innerHTML = `<p>Congratulations ${playerX}</p> <button id="play-again">Play Again!</button>`
         } else if(
                 (gameboard[0]==='O' && gameboard[1]==='O' && gameboard[2]==='O') ||
                 (gameboard[3]==='O' && gameboard[4]==='O' && gameboard[5]==='O') ||
@@ -87,18 +112,39 @@ const Game = (() => {
                 (gameboard[2]==='O' && gameboard[4]==='O' && gameboard[6]==='O')
         ){
             console.log('O Win')
-            displayPlayer.innerHTML = `Congratulations ${playerO}, you won! <button id="play-again">Play Again</button>`
+            displayPlayer.innerHTML = `<p>Congratulations ${playerO}, you won!</p> <button id="play-again">Play Again!</button>`
         } else if (!gameboard.includes('')){
-            displayPlayer.innerHTML = `It is a tie! <button id="play-again">Play Again</button>`
+            displayPlayer.innerHTML = `<p>It is a tie!</p> <button id="play-again">Play Again!</button>`
             console.log('Tie Game')
         }
         return
     }
 
+    const startGame = () => {
+        overlay.classList.add('hide')
+        overoverlay.classList.add('hide')
+        startBtn.classList.add('hide')
+
+        if(typePlayerO === 'real'){
+        blocks.forEach(element => {
+            element.addEventListener('click', () => {
+                Gameboard.renderContent(element.dataset.block);
+            })
+        })} else{
+            Gameboard.renderContent(Math.floor(Math.random() * 9));
+        }
+
+        console.log('hellooo')
+
+        getPlayer();
+    }
+
+
     return {
         changePlayer,
         getPlayer,
-        gameIsOver
+        gameIsOver,
+        startGame
     }
 
 })()
@@ -112,6 +158,8 @@ const Gameboard = (() => {
 
     const renderContent = index => {
         console.log(gameboard)
+
+        // O => player 1    X => player 2
         let moveType
         if(Game.getPlayer()===1) moveType = 'O'
         else moveType = 'X'
@@ -126,25 +174,27 @@ const Gameboard = (() => {
             return acc
         }, ``)
 
-        blocks = document.querySelectorAll('.block')
-
-        blocks.forEach(element => {
-            element.addEventListener('click', () => {
-                renderContent(element.dataset.block);
-            })
-        })
+        console.log(Game.getPlayer())
+        console.log(typePlayerO)
+        if((Game.getPlayer()===1 && typePlayerO === 'cpu') || (Game.getPlayer()!==1 && typePlayerX === 'cpu')){
+            Gameboard.renderContent(Math.floor(Math.random() * 9));
+            }else{
+                blocks = document.querySelectorAll('.block')
+                blocks.forEach(element => {
+                    element.addEventListener('click', () => {
+                        renderContent(element.dataset.block);
+                    })
+                })
+        }
 
         Game.gameIsOver(gameboard)
         
 
-
         if(document.getElementById('play-again')){
             const playAgain = document.getElementById('play-again')
-            console.log('hey')
             playAgain.addEventListener('click', () => {
                 restartGame();
             })
-            
         }
         
     }
@@ -161,8 +211,8 @@ const Gameboard = (() => {
 })()
 
 
-const Players = (playerName) => {
-    return playerName;
+const Players = (playerName, typeUser) => {
+    return {playerName, typeUser};
 }
 
 
